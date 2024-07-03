7bb39a66bd4d6ca884a3bbd5473e434e909b57cc
ckuthyar <ckuthyar@gmail.com>
2024-07-04 04:47:28 +0530

    current time in 8 cities

--- Py/17.py
+++ Py/17.py
@@ -5,16 +5,16 @@ list1=list(data.keys())
 list2=list(data.values())
 responses=[]
 marks=[]
-
-print("Your Quiz starts now")
 s1="What is the capital of "
 s2="?"
 s3="The capital of "
 s4=" is "
-s5="You have scored marks of "
+s5="You have scored marks of"
+
 for i in range(0,len(list1),1):
     responses.append(input(s1+list1[i]+s2))
 print()
+
 for i in range(0,len(list1),1):
     if responses[i]==list2[i]:
         marks.append(10)
